f2e6a65deac7e38eed471ffac843db191466eab9
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-05-20 01:39:10 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,15 +11,10 @@ class Anagram
  def match(look_for_anagram)
 
 
- look_for_anagram.find_all do |word| 
- if word.split("").sort == self.word_check.split("").sort 
-        word 
-      end 
-      end 
-      end 
+
+
 
 end 
-  
 
 
 
